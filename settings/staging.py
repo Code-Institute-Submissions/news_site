be5b669c05207f5ccbf6ee18a0bf0e8be2182319
Darren Francis <dazfrancisllpg@hotmail.com>
2017-10-17 17:12:54 +0100

    cleardb conneciton

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,11 @@
 from .base import *
+import dj_database_url
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 
